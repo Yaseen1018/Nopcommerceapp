192f77dcfd500f318eee9e8b71b1365dc68548e4
yaseen <yaseenhyder1018@gmail.com>
2023-01-03 11:48:20 +0530

    Third commit

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 pytest -s -v -m "sanity" --html=./Reports/sanity.html TestCases/ --browser chrome
-pytest -s -v -m "regression" --html=./Reports/sanity.html TestCases/ --browser Ie
+pytest -s -v -m "regression" --html=./Reports/sanity.html TestCases/ --browser chrome
+
 
 rem pytest -s -v --html=Reports\report6.html TestCases/test_login_ddd.py --browser chrome
 
